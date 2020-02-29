d396a659b0f3a86fbbfcb54aa69351cba4e68204
Theo Roula <theoant24@gmail.com>
2020-02-29 13:07:59 -0500

    RoundManager, Game (verifyEndGame(), MAX_ROUND = 2), PlayerBoard et quelques commentaires

--- src/main/java/uqam/inf5153/game/Game.java
+++ src/main/java/uqam/inf5153/game/Game.java
@@ -13,11 +13,30 @@ public class Game {
 	private DeckManager deckManager;
 	
 	private List<PlayerBoard> playerBoards;
-	
+
+	private static final int MAX_ROUND = 2;
+
 	public Game()
 	{
-		this.roundManager = new RoundManager();
+		this.roundManager = new RoundManager(Game.MAX_ROUND);
 		this.deckManager = new DeckManager();
 		this.playerBoards = new ArrayList<PlayerBoard>();
+	}
+
+	/**
+	 * Verifie si un joueur a complété suffisament d'objectif pour que ce soit le dernier tour de jeu
+	 * @return
+	 */
+	public boolean verifyEndGame()
+	{
+		if(this.roundManager.isLastRound()) return true;
+		boolean callLastRound = false;
+		for(PlayerBoard pb : this.playerBoards)
+		{
+			if(pb.getCountGoalAchieved() == 9) // Dépends du nombre de joueur
+				callLastRound = true;
+		}
+		if(callLastRound) this.roundManager.callLastRound();
+		return callLastRound;
 	}
 }
--- src/main/java/uqam/inf5153/game/managers/RoundManager.java
+++ src/main/java/uqam/inf5153/game/managers/RoundManager.java
@@ -2,4 +2,45 @@ package uqam.inf5153.game.managers;
 
 public class RoundManager {
 
+
+    private boolean isLastRound;
+    private int countRound;
+    private int currentPlayerId;
+    private int maxPlayer;
+
+    public RoundManager(int maxPlayer)
+    {
+        this.isLastRound = false;
+        this.countRound = 0;
+        this.currentPlayerId = 1;
+        this.maxPlayer = maxPlayer;
+    }
+
+    /**
+     * Passe au prochain joueur
+     */
+    public void nextPlayer()
+    {
+        if(this.currentPlayerId > this.maxPlayer)
+        {
+            this.nextRound();
+            this.currentPlayerId = 0;
+        }
+        else this.countRound++;
+    }
+
+    /**
+     * Passe au prochaine tour
+     */
+    public void nextRound() { this.countRound++; }
+
+    /**
+     * Dit que le prochain tour est le dernier
+     */
+    public void callLastRound() { this.isLastRound = true; }
+    public boolean isLastRound() { return this.isLastRound; }
+
+    public int getCurrentPlayerId() { return this.currentPlayerId; }
+
+    public int getCountRound() { return this.countRound; }
 }
